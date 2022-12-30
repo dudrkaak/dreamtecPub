5ccc631b5ab451b938addd6eb7d0b09728788a17
dudrkaak <dudrkaak@naver.com>
2022-12-30 10:37:14 +0900

    test

---
+++ by/js/customize.bxslider.js
@@ -0,0 +1,45 @@
+
+
+$(document).ready(function(){
+    
+    $(".slider").bxSlider({        
+        mode : "horizontal",
+        speed : 700,
+        auto : 3000,
+        startSlide : 1,
+        inginiteLoop : true,
+        adaptiveHeight : true,
+        adaptiveHeightSpeed : 700, 
+        easing : 'ease-in-out',
+        pager : true,
+        pagerType : 'full', 
+        autoControls: false,
+        autoStart: true,
+        pause: 3000, 
+        autoHover: true,
+        autoDelay: 1500,
+        touchEnabled: true,
+        swipeThreshold: 50,
+        minSlides : 1,
+        maxSlides : 1,
+        slideWidth : 1420 
+        
+    });
+
+    toggleCheck();
+});
+function toggleCheck(){
+    var $checkTarget = $(".material-icons");
+    var $checkBox = $("#mui");
+    var checkStatus = false;
+
+    $checkBox.click(function(){
+        checkStatus = !checkStatus;
+        if(checkStatus == true){
+            $checkTarget.text("close");
+        }else{
+            $checkTarget.text("menu");
+        }
+    });
+}
+
